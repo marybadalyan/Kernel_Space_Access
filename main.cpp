9da429bf50e3d827af6a81bad18c996e33ca9663
marybadalyan <marybadalyangdh@gmail.com>
2025-04-21 18:40:48 +0400

    Add ```set_memory_limit()```

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
     #include <signal.h>
     #include <sys/resource.h>
     #include <unistd.h>
+    #include <sys/time.h>
 #endif
 
 bool testArraySize(size_t sizeMBytes) {
@@ -45,12 +46,22 @@ void setup_segfault_handler() {
     sigemptyset(&sa.sa_mask);
     sigaction(SIGSEGV, &sa, nullptr); // a POSIX signal	Sent by the OS when your program violates memory access rules
 }
-#endif
 
+void set_memory_limit(size_t limit_MB) { //if exceeded the memory limit used  memset will get SIGSEGV
+    struct rlimit rlim;
+    rlim.rlim_cur = rlim.rlim_max = limit_MB * 1024 * 1024; // Limit in bytes
+    if (setrlimit(RLIMIT_AS, &rlim) != 0) {
+        perror("setrlimit failed");
+    } else {
+        std::cout << "Set virtual memory limit to " << limit_MB << " MB\n";
+    }
+}
+#endif
 
 int main() {
     #ifndef _WIN32
         setup_segfault_handler();
+        set_memory_limit(4096); // 4 GB cap for testing
     #endif
     
         size_t sizeMBytes = 1 ; //since heap is usualy 
